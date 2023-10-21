fd7a1dc599af9706d83ddb0ff3673916c3b5be36
Qafas <rustam_ussr@mail.ru>
2023-10-21 18:52:21 +0300

    Удаление файлов bootstrap и jquery. Прописывание зависимостей через maven. В HTML файлы зависимости от файлов bootstrap и jquery заменены на зависимости из мавен.

--- services/site/src/main/resources/static/js/directives/header-directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('job4j').directive('header', [function () {
-
-    return {
-        link: function ($scope) {
-            $scope.log_url = CONTEXT;
-        },
-        templateUrl: CONTEXT + 'templates/header.html?v=19.10.2018.1'
-    }
-}]);
