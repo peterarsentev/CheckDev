d29f9165e5ca40e53226611175c0fb25ce8e35de
parsentev <parsentev@yandex.ru>
2023-10-26 17:30:59 +0300

    Поправить скрипты для сборки и обновления на сервере. #301

--- services/notification/sql/update.sql
+++
@@ -1,9 +0,0 @@
--- REG, FORGOT, HR, FORUM_MESSAGE
-alter table template add column type varchar(2000) unique;
-
-update template set type = 'REG' where key = 0;
-update template set type = 'FORGOT' where key = 1;
-update template set type = 'HR' where key = 2;
-update template set type = 'FORUM_MESSAGE' where key = 3;
-
-alter table template drop column key;
